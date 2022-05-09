843ce7837598ad841a31a54e3b39e749e7fad68f
KundanBaidhya <83553416+KundanBaidhya@users.noreply.github.com>
2022-05-09 11:03:28 +0545

    new project

---
+++ bbms.js
@@ -0,0 +1,10 @@
+var tMenu = document.getElementById("tmenu");
+tMenu.style.maxWidth = "0px";
+function toggleMenu(){
+    if(tMenu.style.maxWidth = "0px"){
+        tMenu.style.maxWidth = "130px";
+    }
+    else{
+        tMenu.style.maxWidth = "0px";
+    }
+}
