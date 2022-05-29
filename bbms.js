a58c84b8faeec8748069a7628b55d181178dbb10
KundanBaidhya <83553416+KundanBaidhya@users.noreply.github.com>
2022-05-29 18:12:21 +0545

    without dashboard

--- bbms.js
+++ bbms.js
@@ -1,10 +1,18 @@
 var tMenu = document.getElementById("tmenu");
 tMenu.style.maxWidth = "0px";
+tMenu.style.opacity = 0;
 function toggleMenu(){
-    if(tMenu.style.maxWidth = "0px"){
-        tMenu.style.maxWidth = "130px";
+    if(tMenu.style.maxWidth === "0px"){
+        tMenu.style.maxWidth = "300px";
     }
     else{
         tMenu.style.maxWidth = "0px";
+    }
+
+    if(tMenu.style.maxWidth === "0px"){
+        tMenu.style.opacity = 0;
+    }
+    else{
+        tMenu.style.opacity = 1;
     }
 }
